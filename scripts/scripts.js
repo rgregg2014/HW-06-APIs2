288386e439ebd4c0bb8f1a81823b05bcbcb47b4e
Rachel Gregg <rgregg2014@gmail.com>
2021-07-26 20:27:36 -0400

    Added Bootstrap, CSS, Google Fonts, did some box manipulating, linked jQuery

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+//local storage
+//autocomplete
+//search function
+// pulling data from a server-side API based on user input in search function
+// open weather api (openweathermap.org)
+//Use fetch requests to grab weather data
